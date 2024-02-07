207ae28ac4ed926840c9345111f3260f81c730e2
Tiago Coimbra <tiagocoimbra230@gmail.com>
2024-02-07 08:10:04 -0300

    formulario javascript

--- producao/js/script.js
+++ producao/js/script.js
@@ -68,3 +68,33 @@ galeria.forEach(eventosGaleria);
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+// Formulários JS
+
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'<span style='color: #317A00;'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'<span style='color: #E00000;'>Erro</span> no envio, você pode enviar diretamente no nosso email em: contato@bikcraft.net</p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const data = new FormData(formulario);
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
